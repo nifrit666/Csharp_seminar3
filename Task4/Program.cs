182b55b07a80844e53edf49deea539ac8a1c8207
«Andrey_Istomin» <an.istomin@gmail.com>
2024-02-14 23:56:09 +0600

    Решение Task 4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+void Main()
+{
+    int arraySize = ReadInt("Введите количество чисел в массиве: ");
+    double[] array = GenerateDoubleArray(arraySize, -10, 10);
+    PrintArray(array);
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+double[] GenerateDoubleArray(int size, int leftRange, int rightRange)
+{
+    double[] myArray = new double[size];
+
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        myArray[i] = Math.Round(new Random().NextDouble() + new Random().Next(leftRange, rightRange + 1), 3); // 0 до 10;
+    }
+
+    return myArray;
+}
+
+void PrintArray(double[] arrayForPrint)
+{
+    for (int i = 0; i < arrayForPrint.Length; i++)
+    {
+        System.Console.Write(arrayForPrint[i] + "\t");
+    }
+}
+
+Main();
